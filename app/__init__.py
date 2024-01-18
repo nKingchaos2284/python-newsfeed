48368acaacba35a9d3cebebcecc6450e1e55c606
nKingchaos2284 <jmcabrerae@gmail.com>
2024-01-18 15:40:29 -0800

    update static and templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,17 @@
-from app.routes import home, dashboard, api
+from app.routes import home
 from flask import Flask
-from app.db import init_db
-from app.utils import filters
-
 
 def create_app(test_config=None):
   # set up app config
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
-  app.jinja_env.filters['format_url'] = filters.format_url
-  app.jinja_env.filters['format_date'] = filters.format_date
-  app.jinja_env.filters['format_plural'] = filters.format_plural
-  app.register_blueprint(api)
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
 
-  @app.route('/hello')
-  def hello():
-    return 'hello world'
-
-  # register routes
-  app.register_blueprint(home)
-  app.register_blueprint(dashboard)
-
-  #return app
-
-  init_db(app)
-
-  return app
+@app.route('/hello')
+def hello():
+  return 'hello world'
+# register routes
+app.register_blueprint(home)
+return app
